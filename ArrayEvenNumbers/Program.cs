ca729a86feb23da4af9e9c88df64c843ceab2fcf
Mohammad Mhsoon <mmhsoon@hotmail.com>
2020-05-14 16:56:10 +0200

    Add project files.

---
+++ ArrayEvenNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[10];
+            Console.WriteLine("Enter 10 intergers and press enter after each");
+
+            for (int i = 0; i < 10; i++)
+            {
+                _ = int.TryParse(Console.ReadLine(), out a[i]);
+            }
+
+            Console.WriteLine("the even numbers in the arry");
+            for (int i = 0; i < 10; i++)
+            {
+                if (a[i]%2 ==0)
+                {
+                    Console.WriteLine("[" + a[i] + "]");
+                }
+            }
+
+        }
+    }
+}
